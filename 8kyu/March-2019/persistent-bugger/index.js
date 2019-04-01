cdc21b3de20ef39d787377d9bca636ad938fc98f
Manuel Garaboa <mannygaraboa@gmail.com>
2019-04-01 17:52:58 -0400

    persistent-bugger progress

--- 8kyu/March-2019/persistent-bugger/index.js
+++ 8kyu/March-2019/persistent-bugger/index.js
@@ -1,6 +1,24 @@
-function persistence(num) {
-    if(num.length = 1)
+function persistence(num) 
+{
+  let numString = num.toString();
+  let splitString = numString.split("");
+  var sum = 1;
+  var grandTotal = 0;
+  if(splitString.length == 1)
+  {
+    grandTotal = 0;
+  }
+  while(splitString.length > 1)
+  {
+    for(i = 0; i < splitString.length; i++)
     {
-      return 0;
+      sum = sum * splitString[i];
     }
- }
+    grandTotal++;
+  }
+  console.log(splitString)
+  console.log(sum);
+  return grandTotal;
+}
+
+persistence(num)

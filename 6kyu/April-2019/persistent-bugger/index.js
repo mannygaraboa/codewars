d7bf6c3b5a01bbc37daa7893386e83ecc4118e15
Manuel Garaboa <mannygaraboa@gmail.com>
2019-04-06 09:41:44 -0400

    persistent bugger while loop removed

--- 6kyu/April-2019/persistent-bugger/index.js
+++ 6kyu/April-2019/persistent-bugger/index.js
@@ -8,14 +8,13 @@ function persistence(num)
   {
     grandTotal = 0;
   }
-  while(splitString.length > 1)
-  {
+  
     for(i = 0; i < splitString.length; i++)
     {
       sum = sum * splitString[i];
     }
     grandTotal++;
-  }
+  
   console.log(splitString)
   console.log(sum);
   return grandTotal;

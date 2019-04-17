3a01be21c82fe2be78568293777555a3ece31f2f
Manuel Garaboa <mannygaraboa@gmail.com>
2019-04-16 23:04:16 -0400

    sum-arrays kata solution

---
+++ 8kyu/April-2019/sum-arrays/index.js
@@ -0,0 +1,18 @@
+// Sum Numbers
+function sum (numbers) {
+  "use strict";
+  
+  if(numbers.length == 0)
+  {
+    return 0;
+  }
+  
+  var sum = 0;
+  for(let i = 0; i < numbers.length; i++)
+  {
+    sum = sum + numbers[i];
+  }
+
+  console.log(sum)
+  return sum;
+};

d3795d0fa93e17ad1d816ac6319a257c941a10b2
Manuel Garaboa <mannygaraboa@gmail.com>
2019-04-18 23:29:12 -0400

    Welcome! kata start

---
+++ 8kyu/April-2019/Welcome!/index.js
@@ -0,0 +1,27 @@
+function greet(language) {
+	var language = {
+    
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  };
+  var english = {
+    welcome: 'Welcome'
+  };
+  return greet.language;
+}
+
+greet(english.welcome);

248bf1b172d5d10354d1fb41cb818a177891ee2f
Manuel Garaboa <mannygaraboa@gmail.com>
2019-05-02 01:27:52 -0400

    concept

--- 8kyu/April-2019/remove-exclamations/index.js
+++ 8kyu/April-2019/remove-exclamations/index.js
@@ -1,11 +1,23 @@
-function remove(s,n){
-    for(i = 0; i < n.length; i++)
+// function remove(s,n){
+//     for(i = 0; i < n.length; i++)
+//     {
+//       if(s.charAt[i] == "!")
+//       {
+//         s = s.replace('/!','/');
+//         return s;
+//       }
+//     }
+//   }
+  remove("Hi!", 1)
+
+  function remove(s,n){
+    for(let i = 0; i < n; i++)
     {
-      if(s.charAt[i] == "!")
+      if(s.charAt[i] = "!")
       {
-        s = s.replace('/!','/');
+        s = s.substr(i, i) + "" + s.substr(i + 1);
         return s;
       }
     }
   }
-  remove("Hi!", 1)
+  remove("Hi", 3)

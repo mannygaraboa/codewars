28e687bf90ca6d8b50486c73256214819908f90d
Manuel Garaboa <mannygaraboa@gmail.com>
2019-05-03 20:51:21 -0400

    remove exclamations kata solution

--- 8kyu/April-2019/remove-exclamations/index.js
+++ 8kyu/April-2019/remove-exclamations/index.js
@@ -1,23 +1,35 @@
-// function remove(s,n){
-//     for(i = 0; i < n.length; i++)
-//     {
-//       if(s.charAt[i] == "!")
-//       {
-//         s = s.replace('/!','/');
-//         return s;
-//       }
-//     }
-//   }
-  remove("Hi!", 1)
-
-  function remove(s,n){
-    for(let i = 0; i < n; i++)
+function remove(s,n)
+{
+    let finalString = "";
+    let nCounter = 0;
+    for(let i = 0; i < s.length; i++)
     {
-      if(s.charAt[i] = "!")
+      var currentChar = s.charAt(i);
+      if(nCounter < n)
       {
-        s = s.substr(i, i) + "" + s.substr(i + 1);
-        return s;
+        if(currentChar !== "!")
+        {
+          finalString += currentChar;
+          console.log(finalString);
+        }
+        else if(currentChar == "!")
+        {
+          nCounter++;
+        }
+      }
+      else
+      {
+        finalString += currentChar;
       }
     }
+    console.log(nCounter);
+    return finalString;
   }
-  remove("Hi", 3)
+  remove("!!!Hi!!!!", 5)
+
+// Shorter Solution:
+
+//   function remove(s,n){
+//     for (var i=0;i<n;i++) s=s.replace("!","");
+//     return s;
+//   }

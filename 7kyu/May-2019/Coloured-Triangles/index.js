823bc259ee3bde7a67106b5f6743775c6083ed33
Manuel Garaboa <mannygaraboa@gmail.com>
2019-05-08 22:40:24 -0400

    Coloured Triangles kata solution

--- 7kyu/May-2019/Coloured-Triangles/index.js
+++ 7kyu/May-2019/Coloured-Triangles/index.js
@@ -1,49 +1,49 @@
-function triangle(row) {
-  let newRow = "";
-  for(var i = 0; i < row.length; i++)
-  {
-    var currentColor = row.charAt(i);
-    var nextColor = row.charAt(i + 1);
-    var colorPair = currentColor + nextColor;
-  
-    if(colorPair == "GB" || colorPair == "BG" || colorPair == "RR")
-    {
-      newRow += "R"
-    }
-    else if(colorPair == "RB" || colorPair == "BR" || colorPair == "GG")
-    {
-      newRow += "G"
-    }
-    else if(colorPair == "GR" || colorPair == "RG" || colorPair == "BB")
-    {
-      newRow += "B"
-    }
-  }
-  return newRow;
-}
-
-// return newRow;
-let newerRow = "";
-let newRowLength = newRow.length;
-while(newRowLength > 1)
+function triangle(row) 
 {
-  var currentColor = newRow.charAt(i);
-  var nextColor = newRow.charAt(i + 1);
-  var colorPair = currentColor + nextColor;
-  if(colorPair == "GB" || colorPair == "BG" || colorPair == "RR")
+  originalRowLength = row.length;
+  for(let j = 0; j < originalRowLength; j++)
   {
-    newerRow += "R";
+    let newRow = "";
+    if(row.length == 1)
+    {
+      return row;
+    }
+    else
+    {
+      for(var i = 0; i < row.length; i++)
+      {
+        var currentColor = row.charAt(i);
+        var nextColor = row.charAt(i + 1);
+        var colorPair = currentColor + nextColor;
+        if(colorPair == "GB" || colorPair == "BG" || colorPair == "RR")
+        {
+          newRow += "R";
+        }
+        else if(colorPair == "RB" || colorPair == "BR" || colorPair == "GG")
+        {
+          newRow += "G";
+        }
+        else if(colorPair == "GR" || colorPair == "RG" || colorPair == "BB")
+        {
+          newRow += "B";
+        }
+      }
+    }
+    firstRow = newRow;
+    row = newRow;
+    triangle(row);
+    console.log(row);
+    return firstRow;
   }
-  else if(colorPair == "RB" || colorPair == "BR" || colorPair == "GG")
-  {
-    newerRow += "G";
-  }
-  else if(colorPair == "GR" || colorPair == "RG" || colorPair == "BB")
-  {
-    newerRow += "B";
-  }
-  newRow == newerRow;
-  newRowLength--;
-  console.log(newerRow)
 }
-triangle("BBRG");
+triangle("GRBBRBBRGRGBRGG");
+
+// Shortest Solution:
+
+// const triangle = row => {
+//   let reduced = row.length > 1 ? '' : row;
+//   for (let i = 0; i < row.length - 1; i += 1) {
+//     reduced += row[i] == row[i+1] ? row[i] : 'RGB'.replace(row[i], '').replace(row[i+1], '');
+//   }
+//   return reduced.length > 1 ? triangle(reduced) : reduced;
+// }

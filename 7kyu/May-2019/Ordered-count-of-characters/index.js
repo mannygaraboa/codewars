e90d65b7edf91852ac4d2bc51a2b8a2eea59a165
Manuel Garaboa <mannygaraboa@gmail.com>
2019-05-11 23:51:51 -0400

    ordered count of characters kata start

---
+++ 7kyu/May-2019/Ordered-count-of-characters/index.js
@@ -0,0 +1,11 @@
+var orderedCount = function (text) {
+  let finalArr = [];
+  for(let i = 0; i < text.length; i++)
+  {
+    let currentChar = text.charAt(i);
+    var arr = [currentChar , 0];
+
+  }
+
+  return [];
+}

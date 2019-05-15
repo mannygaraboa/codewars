22d0ad9231e65e0970c0b8a0b051122168cf9e83
Manuel Garaboa <mannygaraboa@gmail.com>
2019-05-14 23:44:51 -0400

    Learned .match()

--- 7kyu/May-2019/Ordered-count-of-characters/index.js
+++ 7kyu/May-2019/Ordered-count-of-characters/index.js
@@ -1,19 +1,28 @@
 var orderedCount = function (text) {
   let finalArr = [];
+  let charCount = 0;
+  // let currentChar = text.charAt(i);
+  // let nextChar = text.charAt(i + 1);
   for(let i = 0; i < text.length; i++)
   {
     let currentChar = text.charAt(i);
-    let nextChar = text.charAt(i + 1);
-    let charCount = 0;
-    var arr = [currentChar , charCount];
-    
-    if(currentChar != nextChar)
+    let arr = [currentChar , charCount];
+    for(let j = 0; j < text.length; j++)
     {
-      charCount++;
+      let nextChar = text.charAt(j + 1);
+      if(currentChar == nextChar)
+      {
+        charCount++;
+        text = text.replace(nextChar, "")
+      }
     }
     finalArr.push(arr)
   }
   return finalArr;
 }
-orderedCount("abcabc")
+orderedCount("aaabbb")
+
+// Use the .match() property
+// Ex: let match = text.match(/a/gi). length 
+// This returns the number of times the letter 'a' appears in the string 'text'
 

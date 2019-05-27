a46362e5f734d897d715cdfa086a770f8cc70062
Manuel Garaboa <mannygaraboa@gmail.com>
2019-05-26 23:44:20 -0400

    persistent bugger kata resume

--- 6kyu/April-2019/persistent-bugger/index.js
+++ 6kyu/April-2019/persistent-bugger/index.js
@@ -3,31 +3,26 @@ function persistence(num)
   let numString = num.toString();
   let splitString = numString.split("");
   var sum = 1;
+  var total = 0;
   var grandTotal = 0;
 
   if(splitString.length == 1)
   {
-    grandTotal = 0;
-  }
-  
-  if(splitString.length > 1)
-  {
-    var grandSum = 10;
-    
-    while(grandSum > 9)
-    {
-      for(i = 0; i < splitString.length; i++)
-      {
-        grandSum = sum * splitString[i];
-        grandTotal++;
-      }
-    }
+    console.log(splitString)
+    console.log(sum);
     return grandTotal;
   }
-  
+
+  while(total < splitString.length)
+  {
+    for(let i = 0; i < splitString.length; i++)
+    {
+      sum = sum * splitString[i];
+      total++;
+    }
+  }
   console.log(splitString)
   console.log(sum);
-  return grandTotal;
+  return total;
 }
-
-persistence(num)
+persistence(4)

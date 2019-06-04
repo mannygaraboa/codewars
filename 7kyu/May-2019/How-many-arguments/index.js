e1a9a09f37d12ed3fc93b47439a6f5c9847f6e83
Manuel Garaboa <mannygaraboa@gmail.com>
2019-06-04 17:18:40 -0400

    how many arguments kata solution

--- 7kyu/May-2019/How-many-arguments/index.js
+++ 7kyu/May-2019/How-many-arguments/index.js
@@ -1,7 +1,19 @@
-function args_count(args)
+function args_count()
 { 
-  let newArray = [];
-  newArray.push(args)
-  console.log(newArray);
+  let arr_count = 0;
+  for(var i = 0; i < arguments.length; i++)
+  {
+    arr_count++;
+  }
+  return arr_count;
 }
 args_count(5,4,2,5,3,3)
+
+// There's a weird "magic" variable you can reference called "arguments":
+
+// It's like an array, but it's not an array. In fact it's so weird that you really shouldn't use it much at all. A common practice is to get the values of it into a real array:
+
+// Shorter Solution:
+// function args_count() {
+//   return arguments.length;
+// }

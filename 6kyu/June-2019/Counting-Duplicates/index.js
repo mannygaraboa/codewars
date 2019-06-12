b240618c6a97ed617ab607f65bce35bc3f9fea6b
Manuel Garaboa <mannygaraboa@gmail.com>
2019-06-11 23:54:45 -0400

    Counting Duplicates bool testing

--- 6kyu/June-2019/Counting-Duplicates/index.js
+++ 6kyu/June-2019/Counting-Duplicates/index.js
@@ -1,22 +1,30 @@
 function duplicateCount(text){
   let count = 0;
-  let arr = [];
   for(let i = 0; i < text.length; i++)
   {
+    let bool = true;
     let currentChar = text.charAt(i);
-    let nextChat = text.charAt(i+1);
+    let nextChar = text.charAt(i+1);
     let re = new RegExp(currentChar, 'g');
     let match = text.match(re).length;
-    if(match > 1 && currentChar != arr)
+    console.log(match);
+    
+    for(let j = 1; j < text.length; j++)
+    {
+      if(currentChar == text.charAt(j))
+      {
+        bool = true;
+      }
+    }
+
+    if(match > 1 && bool == true)
     {
       count++;
-      arr.push(currentChar)
     }
   }
-  console.log(arr);
   return count;
 }
-duplicateCount("hexxxxo");
+duplicateCount("hexxxh");
   
 // Syntax for match(): string.match(RegExp) 
 // 'gi' = case sensitive

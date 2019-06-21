b02ff7b327910bfe6d2e71c582db4a49f2a1c16c
Manuel Garaboa <mannygaraboa@gmail.com>
2019-06-20 23:06:36 -0400

    Conver String to Camel Case Kata Solution

--- 6kyu/June-2019/Convert-String-to-Camel-Case/index.js
+++ 6kyu/June-2019/Convert-String-to-Camel-Case/index.js
@@ -1,14 +1,34 @@
 function toCamelCase(str){
+  let arr = [];
   for(let i = 0; i < str.length; i++)
   {
     let currentChar = str.charAt(i);
     if(currentChar == "-" || currentChar == "_")
     {
-      console.log(currentChar)
-      let newString = str.slice(i, i+1);
-      console.log(newString);
-    } 
+      let nextChar = str.charAt(i+1).toUpperCase();
+      arr.push(nextChar)
+      i++;
+      console.log(nextChar);
+    }
+
+    if(currentChar != "-" && currentChar != "_")
+    {
+      arr.push(currentChar)
+    }
   }
-  return str;
+  let finalString = arr.join("");
+  console.log(finalString)
+  return finalString;
 }
 toCamelCase("hello-world");
+
+// Use .replace();
+
+// Another Solution:
+
+// function toCamelCase(str){
+//   var regExp=/[-_]\w/ig;
+//   return str.replace(regExp,function(match){
+//         return match.charAt(1).toUpperCase();
+//    });
+// }

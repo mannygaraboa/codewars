7b5b2bc1621bc5c7518b5289565207470aa677f0
Manuel Garaboa <mannygaraboa@gmail.com>
2019-07-03 18:55:46 -0400

    encode function completed, decode function progress (needs to decode 3-digit numbers or higher)

--- 6kyu/July-2019/Data-Compression-run-length-encoding/index.js
+++ 6kyu/July-2019/Data-Compression-run-length-encoding/index.js
@@ -1,39 +1,82 @@
+// Encode the string inputted
 function encode(input) {
   let arr = [];
+  let upCaseInput = input.toUpperCase();
   for(let i = 0; i < input.length; i++)
   {
-    let currentChar = input.charAt(i);
-    let upChar = currentChar.toUpperCase();
-    let re = new RegExp(currentChar, 'gi')
-    let match = input.match(re).length;
-    let pairArr = [match, upChar]
+    let currentChar = upCaseInput.charAt(i);
+    let charCount = 1;
+    while(currentChar == upCaseInput.charAt(i+1))
+    { 
+      if(currentChar == upCaseInput.charAt(i+1))
+      {
+        charCount++;
+        i++;
+      }
+    }
+    let pairArr = [charCount, currentChar]
     let pair = pairArr.join("")
     arr.push(pair)
   }
   console.log(arr);
 
-  for(let i = 0; i < arr.length; i++)
-  {
-    for(let j = i; j < arr.length; j++)
-    {
-      if(arr[i] == arr[j+1])
-      {
-        console.log(arr[j+1] + " is getting spliced");
-        arr.splice(j+1,1);
-        console.log(arr);
-      }
-    }
-  }
-  console.log(arr)
   let finalString = arr.join("")
   return finalString;
 }
 
+// Decode the string inputted
 function decode(input) {
-  
-}
-encode("aabbcabccddaax")
-// 2A2B1C1A1B2C2D2A1X
+  let decodeArr = [];
+  let inputArr = [];
+  let newNumArr = [];
+  let upCaseInput = input.toUpperCase();
 
+  // Split all characters in input into an array
+  for(let i = 0; i < input.length; i++)
+  {
+    let currentChar = upCaseInput.charAt(i);
+    let nextChar = upCaseInput.charAt(i+1);
+    inputArr.push(currentChar);
+  }
+
+  // For double digit numbers
+  for(let i = 0; i < input.length; i++)
+  {
+    if(inputArr[i] >= 0 && inputArr[i+1] >= 0)
+    {
+      let newNum = inputArr[i].concat(inputArr[i+1]);
+      console.log(newNum);
+      newNumArr.push(newNum);
+      i++;
+    }
+    else
+    {
+      newNumArr.push(inputArr[i]);
+    }
+  }
+
+  // Actual decoding
+  for(let i = 0; i < newNumArr.length; i++)
+  {
+    let currentChar = newNumArr[i];
+    let nextChar = newNumArr[i+1];
+    if(currentChar >= 1)
+    {
+      for(let j = 0; j < currentChar; j++)
+      {
+        decodeArr.push(nextChar);
+      }
+    }
+  }
+
+  let finalString = decodeArr.join("");
+  console.log(inputArr);
+  console.log(newNumArr);
+  return finalString;
+}
+encode("aaaaaaaaaaaabbcaabbxx");    // Returns '12A2B1C2A2B2X'
+decode("10j4b12z2D");               // Returns 'JJJJJJJJJJBBBBZZZZZZZZZZZZDD'
+
+// Previous Method:
 // let re = new RegExp(currentChar, 'gi')
 // let match = text.match(re).length
